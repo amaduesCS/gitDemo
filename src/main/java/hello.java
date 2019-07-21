28d0138d09de3842803ec3dd4af34e244a7bc23f
Amadeus <wangyihengcs@hotmail.com>
2019-07-21 19:00:08 +0800

    user1 2st commit

--- src/main/java/hello.java
+++ src/main/java/hello.java
@@ -1,7 +1,8 @@
 public class hello {
     public static void main(String[] args) {
-        System.out.println("hello");
+        System.out.println("hellovvv");
         System.out.println("hello2");
         System.out.println("hello3");
+        System.out.println(111);
     }
 }
