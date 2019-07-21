87ab7b4bca016b63626683c303430fd6e226aef6
Amadeus <wangyihengcs@hotmail.com>
2019-07-21 19:34:31 +0800

    Merge branch 'master' of https://github.com/amaduesCS/gitDemo
    # Conflicts:
    #	src/main/java/hello.java

--- src/main/java/hello.java
+++ src/main/java/hello.java
@@ -5,6 +5,6 @@ public class hello {
         System.out.println("hello3");
         System.out.println(2222222);
         System.out.println(112);
-        System.out.println(116);
+        System.out.println(118);
     }
 }
